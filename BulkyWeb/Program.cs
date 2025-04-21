0c48e9029b78b676171b510782a76e8dd66a2837
Rahithya <rahithyakilaru2025@gmail.co>
2025-04-21 04:46:39 -0500

    Switched to SQLite for Render deployment

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
@@ -33,7 +33,15 @@ var app = builder.Build();
 
 
 
-app.UseDeveloperExceptionPage();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
 
 app.UseHttpsRedirection();
@@ -44,6 +52,8 @@ app.UseRouting();
 app.UseAuthentication();
 
 app.UseAuthorization();
+
+
 
 app.MapRazorPages();
 app.MapControllerRoute(
